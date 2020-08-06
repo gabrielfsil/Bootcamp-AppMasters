e56e92503a55a35707791e34ce93f2d13b370d74
gabrielfsil <gabrielfsil264@gmail.com>
2020-08-06 17:16:15 -0300

    Updates

---
+++ precache-manifest.cdcaff879744ecced1fa27c6446b27a9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "e8fcb19490797437e8a1",
+    "url": "/Bootcamp-AppMasters/static/css/main.a07be29d.chunk.css"
+  },
+  {
+    "revision": "e8fcb19490797437e8a1",
+    "url": "/Bootcamp-AppMasters/static/js/main.e8fcb194.chunk.js"
+  },
+  {
+    "revision": "41035da17106aa29451d",
+    "url": "/Bootcamp-AppMasters/static/js/runtime~main.41035da1.js"
+  },
+  {
+    "revision": "d3573b2ccea7307003c5",
+    "url": "/Bootcamp-AppMasters/static/css/2.adaed672.chunk.css"
+  },
+  {
+    "revision": "d3573b2ccea7307003c5",
+    "url": "/Bootcamp-AppMasters/static/js/2.d3573b2c.chunk.js"
+  },
+  {
+    "revision": "8e9f0a3c5578a20733d5bad0e51c91fb",
+    "url": "/Bootcamp-AppMasters/static/media/sidebar-1.8e9f0a3c.jpg"
+  },
+  {
+    "revision": "d06fb4b929f368fae66987171a358f8a",
+    "url": "/Bootcamp-AppMasters/index.html"
+  }
+];
